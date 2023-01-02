f271e2261ab874bb587952c054e5e3a0f4bec18e
Tyler <Tyler29475@gmail.com>
2023-01-02 18:15:17 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Extra credit?
+
+# Given a list of numbers, calculate their sum using a for loop.
+
+# x = [42, 8, 7, 1, 0, 124, 8897, 555, 3, 67, 99]
+# Sum = sum(x)
+# print(Sum)
+
+# Actual solution;
+
+x = [42, 8, 7, 1, 0, 124, 8897, 555, 3, 67, 99]
+nums = 0
+for i in x:
+    nums += i
+print(nums)
